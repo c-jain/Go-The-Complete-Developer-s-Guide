044f7f3f6f2179a306d19c043ef81a7b41eb4c78
Chakshu Jain <chakshujain1997@gmail.com>
2023-09-18 21:06:32 +0530

    joining a slice of strings

--- cards/main.go
+++ cards/main.go
@@ -3,12 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// cards := newDeck()
-
-	// hand, remaining := deal(cards, 5)
-
-	// hand.print()
-	// remaining.print()
-	greeting := "Hi there!"
-	fmt.Println([]byte(greeting))
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
