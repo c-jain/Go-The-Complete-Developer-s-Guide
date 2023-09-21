ca5b3901483bf57af9abb9c93c88fd9ea2392e16
c-jain <chakshujain1997@gmail.com>
2023-09-21 17:50:51 +0530

    structs with pointers

--- structs/main.go
+++ structs/main.go
@@ -23,13 +23,14 @@ func main() {
 		},
 	}
 
-	jim.updateName("jimmy")
+	jimPointer := &jim
+	jimPointer.updateName("jimmy")
 	jim.print()
 
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
